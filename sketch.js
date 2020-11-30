688a093d92fe329948472992584d72464a47a3d4
simplyjasleen <kjasleen.arora@gmail.com>
2020-11-30 13:37:11 +0530

    No images

--- sketch.js
+++ sketch.js
@@ -3,19 +3,22 @@ var database;
 var foodS,foodStock;
 var dogImg;
 var dogImg1
-
+/*
 function preload()
 {
   dogImg1 = loadImage("images/dogImg.png");
   dogImg2 = loadImage("images/dogImg1.png");
 }
+*/
 
 function setup() {
   createCanvas(500, 500);
-  dog = createSprite(250,250,2,2);
-  dog.addImage("dog1",dogImg1);
+  database = firebase.database();
 
- database = firebase.database();
+  dog = createSprite(250,250,2,2);
+ // dog.addImage("dog1",dogImg1);
+
+ 
 
  foodStock = database.ref('food');
  foodStock.on("value",readStock);
@@ -25,17 +28,16 @@ function setup() {
 
 function draw() {  
 background(46,139,87);
-drawSprites(); 
-  
+drawSprites();
+
+textSize(15);
+fill("red");
+text("note : press up arrow key to feed dog milk" + foodS,100,100);
+
   if(keyWentDown(UP_ARROW)){
      writeStock(foodS);
-     dog.addImage("dog2",dogImg2);
-
+    // dog.addImage("dog2",dogImg2);
     
-
-     Text("note : press up arrow key to feed dog milk",450,75);
-     textSize(15);
-     fill("red");
 
   }
   
@@ -43,29 +45,20 @@ drawSprites();
 }
 function readStock(data){
   foodS = data.val();
-
-
-
 }
 
 function writeStock(x){
 
-  if(x<=0){
-    x=0;
-
-  }else{
-   x=x-1; 
-
-  }
-  
- database.ref('/').update
- ({
-   
-  food:x
-
- })
-
-
+      if(x<=0){
+        x=0;
+      }else{
+      x=x-1; 
+    }
+      
+    database.ref('/').update
+    ({
+      'food':x
+    })
 }
 
 
